48850466234afd61396a3d26251415fa621ea4b2
Sam Ashtar <samashtarm@gmail.com>
2018-08-25 10:25:39 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
 def alphabetize(arr)
- 
+  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   arr.sort_by do |word|
     word.split('').collect do |letter|
-      arr.index(letter)
+      esperanto_alphabet.index(letter)
     end
   end
 end
